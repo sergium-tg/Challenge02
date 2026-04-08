655f166e3442d57c8e5e6be22fff30f6922690e5
sergium-tg <sergio.talero@uao.edu.co>
2026-04-08 09:49:29 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'challenge07',
+  webDir: 'dist'
+};
+
+export default config;
